5d359c52971a2c8c14566f04c03e33479a2b84ee
Alex Glica <4258881+Krieger351@users.noreply.github.com>
2024-11-16 14:39:58 -0500

    remove dead code

--- src/lib/top_quartile.rs
+++
@@ -1,6 +0,0 @@
-pub fn top_quartile<T: PartialOrd + Copy>(vec: &Vec<T>) -> T {
-    let mut sorted_vec = vec.clone();
-    sorted_vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
-    let index = (sorted_vec.len() as f32 * 0.75).ceil() as usize - 1;
-    sorted_vec[index]
-}
